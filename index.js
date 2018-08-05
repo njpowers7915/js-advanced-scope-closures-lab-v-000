40de85a34c66ac7d1bfe6bde166b06a5631535e2
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 20:07:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange) {
   return function(start, end) {
     if ((end - start) <= blockRange) {
       return `within range by ${blockRange - (end - start)}`
-    } else if ((end - start) > blockRange) {
+    } else {
       return `${(end - start) - blockRange} blocks out of range`
     }
   }
