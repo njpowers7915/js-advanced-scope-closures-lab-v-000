2ed7f7b2581d4a24be2d936dc5c8abc7e79ed862
Nicholas Powers <njpowers7915@gmail.com>
2018-08-05 20:10:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function(start, end) {
+    startInt = parseInt(start.slice(0, -2))
+    endInt = parseInt(end.slice(0, -2))
     if ((end - start) <= blockRange) {
       return `within range by ${blockRange - (end - start)}`
     } else {
